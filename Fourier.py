3cceff7d1543caf9e78caff142fe34c3fb5a851a
ValdRichard <ricardovaldivia2003@gmail.com>
2026-05-30 17:04:44 -0300

    Correcciones esteticas

--- Fourier.py
+++ Fourier.py
@@ -1,32 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-L = 1  # Valor arbitrario
-
+L = 1
 x = np.linspace(-L, L, 1000)
 
-# tu función definida a trozos
 f = np.where(x < 0, 0, x)
 
-plt.plot(x, f)
-plt.xlabel('x')
-plt.ylabel('f(x)')
-plt.title('f(x)')
-plt.grid(True)
-plt.show()
-
-x = np.linspace(-3*L, 3*L, 3000)
-
-# periodicidad manual: llevás x al intervalo [-L, L]
-x_mod = ((x + L) % (2*L)) - L
-f = np.where(x_mod < 0, 0, x_mod)
-
-plt.plot(x, f)
-plt.grid(True)
-plt.show()
-
 N = 20
-S = np.full_like(x, L/4)  # término a0/2
+S = np.full_like(x, L/4)  # a0/2
 
 for n in range(1, N+1):
     an = L/(n**2 * np.pi**2) * ((-1)**n - 1)
@@ -34,7 +15,10 @@ for n in range(1, N+1):
     S += an * np.cos(n * np.pi * x / L) + bn * np.sin(n * np.pi * x / L)
 
 plt.plot(x, f, label='f(x)', linewidth=2)
-plt.plot(x, S, label=f'Serie N={N}', linestyle='--')
+plt.plot(x, S, label=f'Serie de Fourier N={N}', linestyle='--')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Serie de Fourier')
 plt.legend()
 plt.grid(True)
 plt.show()
